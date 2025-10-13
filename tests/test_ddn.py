509f7629b360d5b345e701d304126322415b51be
lucidrains <lucidrains@gmail.com>
2025-10-13 09:07:51 -0700

    make sure it works for other types of data than images, fix indexing in split prune

--- tests/test_ddn.py
+++ tests/test_ddn.py
@@ -72,3 +72,29 @@ def test_patches():
     # after much training
 
     assert sampler.forward_for_codes(features[:3], codes[:3]).shape == (3, 3, 32, 32)
+
+def test_non_image():
+    from discrete_distribution_network.ddn import GuidedSampler
+
+    sampler = GuidedSampler(
+        dim = 32,
+        codebook_size = 10,
+        network = nn.Linear(32, 16),
+        min_total_count_before_split_prune = 1,
+        crossover_top2_prob = 1.,
+    )
+
+    features = torch.randn(10, 32)
+    query = torch.randn(10, 16)
+
+    out, codes, commit_loss = sampler(features, query)
+
+    assert out.shape == query.shape
+    assert codes.shape == (10,)
+    assert commit_loss.numel() == 1
+
+    sampler.split_and_prune_()
+
+    # after much training
+
+    assert sampler.forward_for_codes(features[:3], codes[:3]).shape == (3, 16)
